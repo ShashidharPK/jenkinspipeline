25932926294a12ff0d9d638542c3e7c9ef9a7e97
sriramravi <rsriram.10689@gmail.com>
2022-11-24 17:24:14 +0530

    Add files via upload

---
+++ vars/snyk.groovy
@@ -0,0 +1,41 @@
+def call(String repoUrl, string severity, string org, string proj, string environment, string lifecycle, string criticality) {
+	pipeline {
+    agent any
+
+    stages {
+        stage('Checkout Repo') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: '49174964-c138-49e8-bb2d-5daaab4ba293', url: "${repoUrl}"]]])
+            }
+        }
+        stage('Install Dependencies') {
+            steps {                
+                sh 'npm install'
+            }
+        }
+        stage('Run Snyk') {
+            steps {
+				snykSecurity  (
+                    additionalArguments: "--remote-repo-url=${repoUrl} --project-environment=${environment} --project-lifecycle=${lifecycle} --project-business-criticality=${criticality}",
+                    severity: "${severity}", 
+                    organisation: "${org}",
+                    projectName: "${proj}",
+                    snykInstallation: 'snyk', 
+                    snykTokenId: 'snyk-api-token'
+                )                    
+			}
+        }
+        stage('IAC Scan') {
+            steps {
+                    withCredentials([string(credentialsId: 'snyk-token', variable: 'TOKEN')])  {
+                    sh '''
+                        set +e
+                        snyk auth ${TOKEN}
+                        snyk iac test --report
+                        '''
+                    }
+                }            
+			}
+        }
+	}
+}
