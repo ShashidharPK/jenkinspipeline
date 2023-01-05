9620a20f17411f97c37a511728b54f9dd3d136da
sriramravi <rsriram.10689@gmail.com>
2023-01-05 15:33:46 +0530

    Update snykSecurityScan.groovy

--- vars/snykSecurityScan.groovy
+++ vars/snykSecurityScan.groovy
@@ -1,24 +1,42 @@
+/* To connect with the snyk dashboard SNYK_TOKEN should be set up in the Jenkins environment as a secret variable
+*  For executing the snyk.groovy there are some mandatory variables that needs to be passed from jenkinsfile
+*  Required Variables where the build fails when the values is not passed
+*  repoUrl: URL of the repository that needs to be scanned
+*  severity: Severity of the vulnerabilities where the scans fail
+*  orgId: Snyk Organization id
+*  projectName: Name of the project as it is displayed in Snyk
+*  dockerImage: Image which needs to be scanned
+*  imageTag: Tag of the docker image
+*  performAppAnalysis: Set to true, SCA and SAST analysis are performed
+*  iacAnalysis: Set to true, IAC scans are performed
+*  containerAnalysis: Set to true, Container scans are performed
+*  Optional Variables where the default values are mentioned in the snyk.groovy
+*  environment: Environment tag displayed in snyk
+*  lifecycle: Lifecycle tag displayed in snyk
+*  businessCriticality: Business criticality tag displayed in snyk
+*/
 def call(Map snykConfig) {
 
-    String repoUrl = snykConfig.repoUrl
-    String severity = snykConfig.severity
-    String orgId = snykConfig.orgId
-    String projectName = snykConfig.projectName    
-    String dockerImage = snykConfig.dockerImage
-    String imageTag = snykConfig.imageTag
-    Boolean performAppAnalysis = snykConfig.performAppAnalysis //Performs SCA and SAST analysis if performAppAnalysis is set to true
-    Boolean iacAnalysis = snykConfig.iacAnalysis    
-    Boolean containerAnalysis = snykConfig.containerAnalysis
+    String repoUrl = snykConfig.repoUrl ? "${snykConfig.repoUrl}" : ""
+    String severity = snykConfig.severity ? "${snykConfig.severity}" : ""
+    String orgId = snykConfig.orgId ? "${snykConfig.orgId}" : ""
+    String projectName = snykConfig.projectName ? "${snykConfig.projectName}" : ""
+    String dockerImage = snykConfig.dockerImage ? "${snykConfig.dockerImage}" : ""
+    String imageTag = snykConfig.imageTag ? "${snykConfig.imageTag}" : "latest"
+    Boolean performAppAnalysis = snykConfig.performAppAnalysis ? "${snykConfig.performAppAnalysis}" : "false" //Performs SCA and SAST analysis if performAppAnalysis is set to true
+    Boolean iacAnalysis = snykConfig.iacAnalysis ? "${snykConfig.iacAnalysis}" : "false"
+    Boolean containerAnalysis = snykConfig.containerAnalysis ? "${snykConfig.containerAnalysis}" : "false"
     //Environment, lifecycle and business criticality are tags provided for each project in Snyk. Null value will be taken by default if the value is not provided
     String environment = snykConfig.environment ? "${snykConfig.environment}" : "" 
     String lifecycle = snykConfig.lifecycle ? "${snykConfig.lifecycle}" : ""
     String businessCriticality = snykConfig.businessCriticality ? "${snykConfig.businessCriticality}" : ""
+    String appFindings = snyk.appFindings ? "${snykConfig.appFindings}" : "false"
 
      
         stage('executeScaAnalysis') {
 		 
 		    if (performAppAnalysis == true) {
-                catchError(buildResult: 'SUCCESS')  {
+                catchError(buildResult: "${appFindings}")  {
                    	withCredentials([string(credentialsId: 'snyk-token', variable: 'TOKEN')])  {
                    	sh """
                        	set +e                        
@@ -32,7 +50,7 @@ def call(Map snykConfig) {
         
         stage('executeSastAnalysis') {
 		    if ( performAppAnalysis == true ) {
-                catchError(buildResult: 'SUCCESS')  {
+                catchError(buildResult: "${appFindings}")  {
                     withCredentials([string(credentialsId: 'snyk-token', variable: 'TOKEN')])  {
                     sh """
                         set +e
@@ -47,7 +65,7 @@ def call(Map snykConfig) {
         stage('executeIacAnalysis') {
 	      
 			if ( iacAnalysis == true ) {
-                catchError(buildResult: 'SUCCESS')  {
+                catchError(buildResult: "${appFindings}")  {
                     withCredentials([string(credentialsId: 'snyk-token', variable: 'TOKEN')])  {
                     sh """
                         set +e                        
@@ -61,7 +79,7 @@ def call(Map snykConfig) {
         stage('executeContainerAnalysis'){
 		    // Checks for application vulnerability from the container images
 		    if ( containerAnalysis == true && performAppAnalysis == true ) {
-                catchError(buildResult: 'SUCCESS')  {
+                catchError(buildResult: "${appFindings}")  {
                     withCredentials([string(credentialsId: 'snyk-token', variable: 'TOKEN')])  {
                             sh """
                                 set +e
@@ -72,7 +90,7 @@ def call(Map snykConfig) {
                     }
                 }
             if ( containerAnalysis == true && performAppAnalysis == false ) {
-                catchError(buildResult: 'SUCCESS')  {
+                catchError(buildResult: "${appFindings}")  {
                     withCredentials([string(credentialsId: 'snyk-token', variable: 'TOKEN')])  {
                             sh """
                                 set +e
